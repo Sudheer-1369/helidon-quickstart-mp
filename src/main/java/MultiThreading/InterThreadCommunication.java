3bd5dbf6ce8520550fabafc78af9f18ba0ebf716
sudheer patnana <sudheerpatnana45@gmail.com>
2022-06-07 11:18:34 +0530

    For branch 4120

--- src/main/java/MultiThreading/InterThreadCommunication.java
+++ src/main/java/MultiThreading/InterThreadCommunication.java
@@ -4,86 +4,66 @@
 
 package MultiThreading;
 
-import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class InterThreadCommunication {
-    public static void main(String[] args) throws InterruptedException {
-        final PC pc = new PC();
+class Customer {
+    private int amount;
 
-        // Create a thread object that calls pc.produce()
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    pc.produce();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        // Create another thread object that calls
-        // pc.consume()
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    pc.consume();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        // Start both threads
-        t1.start();
-        t2.start();
-
-        // t1 finishes before t2
-        t1.join();
-        t2.join();
+    public Customer(int amount) {
+        this.amount = amount;
     }
 
-    // PC (Produce Consumer) class with produce() and
-    // consume() methods.
-    public static class PC {
-        // Prints a string and waits for consume()
-        public void produce() throws InterruptedException {
-            // synchronized block ensures only one thread
-            // running at a time.
-            synchronized (this) {
-                System.out.println("producer thread running");
+    public synchronized void withDraw(int amount) {
 
-                // releases the lock on shared resource
+        System.out.println("Ready to withDraw");
+
+        if (this.amount < amount) {
+            try {
                 wait();
-
-                // and waits till some other method invokes notify().
-                System.out.println("Resumed");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
+        System.out.println("Amount withdrawn");
+    }
 
-        // Sleeps for some time and waits for a key press. After key
-        // is pressed, it notifies produce().
-        public void consume() throws InterruptedException {
-            // this makes the produce thread to run first.
-            Thread.sleep(1000);
-            Scanner s = new Scanner(System.in);
+    public synchronized void deposite(int amount) {
 
-            // synchronized block ensures only one thread
-            // running at a time.
-            synchronized (this) {
-                System.out.println("Waiting for return key.");
-                s.nextLine();
-                System.out.println("Return key pressed");
+        System.out.println("Ready to deposit the money");
 
-                // notifies the produce thread that it
-                // can wake up.
-                notify();
+        this.amount += amount;
+        notify();
+        System.out.println("Money deposited");
+    }
 
-                // Sleep
-                Thread.sleep(2000);
+}
+
+public class InterThreadCommunication {
+
+    static Customer customer = new Customer(50000);
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Thread thread1 = new Thread(() -> {
+            customer.withDraw(95000);
+            customer.deposite(20000);
+        });
+
+        executor.execute(thread1);
+
+        executor.shutdown();
+
+        try {
+            if (executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
             }
+        } catch (InterruptedException e) {
+            executor.shutdown();
+            Thread.currentThread().interrupt();
         }
     }
 }
-
