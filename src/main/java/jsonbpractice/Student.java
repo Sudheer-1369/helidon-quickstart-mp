1b689ecd6ce8c770bb7d15679382f61a3a4a7a61
sudheer patnana <sudheerpatnana45@gmail.com>
2022-08-02 10:43:35 +0530

    JsonbPractice

---
+++ src/main/java/jsonbpractice/Student.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021 - 2022, Sudheer Kumar Patnana, All rights reserved.
+ */
+
+package jsonbpractice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.JsonbPropertyOrder;
+import javax.json.bind.annotation.JsonbTransient;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonbPropertyOrder({"lastName","firstName","age","section,fee"})
+public class Student {
+
+
+    @JsonbProperty("Last_Name")
+    private String lastName;
+    @JsonbProperty("First_Name")
+    private String firstName;
+    @JsonbProperty("Age")
+    private int age;
+    @JsonbTransient
+    private int fee;
+    @JsonbProperty("Class_Section")
+    private String section;
+
+}
